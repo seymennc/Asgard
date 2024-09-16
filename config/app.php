3cf107a142ce52996fd59bcd685f4149773e5c67
Seymen Çayır <seymencayir429@gmail.com>
2024-09-17 01:55:32 +0300

    Debug Mode Added

--- config/app.php
+++ config/app.php
@@ -2,5 +2,25 @@
 
 return [
 
-    'base_path' => '\\' . env('BASE_PATH'),
+    /*------------------------------------------------
+    | Base Path Directory
+    |-------------------------------------------------
+    |
+    | This is the base path directory of the project.
+    |
+    --------------------------------------------------*/
+
+    'base_path' => '\\' . env('BASE_PATH', '\\'),
+
+
+    /*------------------------------------------------
+    | Debug Mode
+    |-------------------------------------------------
+    |
+    | When this mode is enabled, the system provides
+    | more detailed error messages and stack
+    | trace information.
+    |
+    --------------------------------------------------*/
+    'debug' => env('DEBUG', true),
   ];
