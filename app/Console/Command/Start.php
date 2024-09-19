dd60e4192a33d91a8f5eeac8f3d0af9860319868
Seymen Çayır <seymencayir429@gmail.com>
2024-09-20 00:41:10 +0300

    Start file entegrated env file

--- app/Console/Command/Start.php
+++ app/Console/Command/Start.php
@@ -9,33 +9,28 @@ class Start
     const COLOR_RESET = "\033[0m";
     const COLOR_GREEN = "\033[32m";
 
-    public function handle()
+    public function handle(): void
     {
-        $host = '127.0.0.1';
-        $port = 8000;
+        $host = env('SERVER_HOST', 'localhost');
+        $port = env('SERVER_PORT', 8000);
 
         $this->info('INFO');
         echo "PHP Server is " . $this->success('running') . " on [http://{$host}:{$port}]" . PHP_EOL . PHP_EOL;
         echo 'Quit the server with CONTROL-C.' . PHP_EOL;
 
         $output = [];
-        exec("php -S {$host}:{$port} -t storage 2>&1", $output);
+        exec("php -S {$host}:{$port} -t assets 2>&1", $output);
     }
 
-    protected function info($message)
+    protected function info($message): void
     {
-        $boxWidth = strlen($message) - 4; // Kutu genişliği (mesaj uzunluğu + padding)
+        $boxWidth = strlen($message) - 4;
         echo PHP_EOL;
-        // Üst sınır
         echo self::COLOR_BLUE_BG . self::COLOR_WHITE . str_repeat(' ', $boxWidth) . self::COLOR_RESET;
-
-        // Mesaj içeriği
         echo self::COLOR_BLUE_BG . self::COLOR_WHITE . " " . $message . " " . self::COLOR_RESET . " ";
-
-        // Alt sınır
         echo self::COLOR_BLUE_BG . self::COLOR_WHITE . str_repeat(' ', $boxWidth) . self::COLOR_RESET;
     }
-    protected function success($message)
+    protected function success($message): string
     {
         return self::COLOR_GREEN . $message . self::COLOR_RESET;
     }
