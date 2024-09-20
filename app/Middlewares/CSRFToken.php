3846cdd2bb108c20dbd6b4c5ebebff770df6780e
Seymen Çayır <seymencayir429@gmail.com>
2024-09-20 05:39:02 +0300

    CSRF Token System added.

---
+++ app/Middlewares/CSRFToken.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Middlewares;
+
+use Asgard\system\Exceptions\Method\CSRFTokenException;
+use Asgard\system\Request;
+
+class CSRFToken
+{
+    public function __construct()
+    {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    }
+    /**
+     * @throws CSRFTokenException
+     */
+    public function handle(Request $request, \Closure $next)
+    {
+        if ($request->getMethod() === 'post') {
+            if (!$request->_csrf_token) {
+                die(throw new CSRFTokenException("CSRF token is missing"));
+            }
+            $this->verify_csrf_token($request->_csrf_token);
+        }
+        return $next($request);
+    }
+
+    /**
+     * @throws CSRFTokenException
+     */
+    function verify_csrf_token($token): void
+    {
+        if (!isset($_SESSION['_csrf_token']) || $token !== $_SESSION['_csrf_token']) {
+            throw new CSRFTokenException("Invalid CSRF token");
+        }
+    }
+
+}
