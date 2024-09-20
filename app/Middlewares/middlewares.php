75fc874dfb6112f05133ed1ce64e93fa9f51d2ae
Seymen Çayır <seymencayir429@gmail.com>
2024-09-20 05:39:37 +0300

    Default middlewares added

--- app/Middlewares/middlewares.php
+++ app/Middlewares/middlewares.php
@@ -6,8 +6,10 @@ use Asgard\app\Kernel;
 
 class middlewares extends Kernel
 {
+    protected static array $default = [
+        'csrf'
+    ];
     protected static array $middleware = [
-        'deneme' => \Asgard\app\Middlewares\DenemeMiddleware::class,
-        'ok'    => \Asgard\app\Middlewares\OkMiddleware::class,
+        'csrf' => \App\Middlewares\CSRFToken::class,
     ];
 }
